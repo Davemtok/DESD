e1d91d974cbd21cc5eba073bde89cdbbddce3f58
Davemtork <124881746+Davemtok@users.noreply.github.com>
2024-03-18 21:41:31 +0000

    Attempted Register and Verification
    I made the signup page with the email verification but I may need to change it to sign in with google instead as the method being used right now is not as secure and they are going to discontinue it soon

--- SmartCare_app_api/smartcare_app/views.py
+++ SmartCare_app_api/smartcare_app/views.py
@@ -10,6 +10,10 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, logout
 from django.contrib.auth import login
+from smartcare_app_api import settings
+from django.core.mail import send_mail
+
+
 def home(request):
     return render(request, "authentication/index.html")
 
@@ -22,11 +26,36 @@ def signup(request):
         pass1 = request.POST['pass1']
         pass2 = request.POST['pass2']
         
+        if User. objects.filter(username=username).exists():
+            messages.error(request, "Username already exists!! Please try another username.")
+            return redirect('home')
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Email already exists!! Please try another email.")
+        
+        if len(username) > 10:
+            messages.error(request, "Username must be under 10 characters!!")
+            
+        if pass1 != pass2:
+            messages.error(request, "Passwords do not match!!")
+        
+        if not username.isalnum():
+            messages.error(request, "Username should only contain letters and numbers!!")
+            return redirect('home')
+        
+        
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
         myuser.save()
         messages.success(request, "Your Account has been created succesfully!! Please check your email to confirm your email address in order to activate your account.")
+        
+        # Welcome Email
+        subject = "Welcome to SmartCare"
+        message = "Hello " + myuser.first_name + "!!\n" + "Welcome to SmartCare.\n Thank you for visiting our website \n We have also sent you a confirmation email. Please confirm your email address to activate your account."
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [myuser.email]
+        send_mail(subject, message, from_email, to_list, fail_silently=False)
+        
         
         return redirect('signin')
         
